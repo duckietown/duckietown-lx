1128700da1ed105e3e2522ccc471f0e7f75d808c
liampaull <paulll@iro.umontreal.ca>
2021-09-15 11:28:44 -0400

    ros template

---
+++ ros-template/launchers/run_pedestrian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+source /environment.sh
+
+source /code/catkin_ws/devel/setup.bash --extend
+source /code/submission_ws/devel/setup.bash --extend
+source /code/solution/devel/setup.bash --extend
+
+roslaunch --wait agent agent_node.launch &
+roslaunch --wait car_interface all.launch veh:=$VEHICLE_NAME &
+roslaunch --wait duckietown_demos lane_following.launch veh:=$VEHICLE_NAME
+
