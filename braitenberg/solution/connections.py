be2c8fe5a3b235ce008975d75c331c2572a0341c
Charlie Gauthier <charlie.gauthier@umontreal.ca>
2021-11-03 15:56:30 -0400

    cleanup braitenberg

--- braitenberg/solution/connections.py
+++ braitenberg/solution/connections.py
@@ -7,12 +7,10 @@ def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
     res[100:150, 100:150] = 1
     res[300:, 200:] = 1
-
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
     res = np.zeros(shape=shape, dtype="float32")  # write your function instead of this one
     res[100:150, 100:300] = -1
-
     return res
