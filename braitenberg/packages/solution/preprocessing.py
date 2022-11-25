2f0a3c3bbf201db9d28d1d1f85aa8334fc5229b8
Andrea F. Daniele <afdaniele@duckietown.com>
2022-11-25 00:44:26 -0500

    added some notes

--- braitenberg/packages/solution/preprocessing.py
+++ braitenberg/packages/solution/preprocessing.py
@@ -9,5 +9,4 @@ def preprocess(image_rgb: np.ndarray) -> np.ndarray:
     """ Returns a 2D array """
     hsv = cv2.cvtColor(image_rgb, cv2.COLOR_RGB2HSV)
     mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
-    #     masked = cv2.bitwise_and(image, image, mask=mask)
     return mask
