48e02a32efae529ebe88b330311a9ec353c872af
davide <Tuxliri@users.noreply.github.com>
2023-04-19 11:11:59 -0600

    Removed github credentials script

--- dd21-ukf-lx/GITHUB_CREDENTIALS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# YOUR GITHUB TOKEN CREDENTIALS
-GITHUB_USERNAME=YOUR_USERNAME_HERE
-GITHUB_TOKEN=YOUR_TOKEN_HERE
-
-## DO NOT MODIFY BELOW ##
-git config --system url."https://$GITHUB_USERNAME:$GITHUB_TOKEN@github.com/".insteadOf "https://github.com/"
